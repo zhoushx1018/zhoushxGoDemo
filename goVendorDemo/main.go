53af76d32c6cdb0a83c0b5587fadf0e0bf6dba3f
zhoushengxian <zhoushengxian@richinfo.cn>
2017-02-17 17:17:40 +0800

    新增 项目 goVendorDemo

---
+++ goVendorDemo/main.go
@@ -0,0 +1,16 @@
+//使用vendor管理Golang项目依赖
+//http://www.tuicool.com/articles/NjMzIbJ
+
+package main
+
+import (
+	"fmt"
+
+	"xx.org/dep"
+)
+
+func main() {
+	//    fmt.Printf("Hello, world!\n")
+	fmt.Printf("1111111Hello, world!\n")
+	external.Show()
+}
---
+++ goVendorDemo/vendor/xx.org/dep/external.go
@@ -0,0 +1,9 @@
+package external
+
+import (
+	"fmt"
+)
+
+func Show() {
+	fmt.Println("call from external dep")
+}
