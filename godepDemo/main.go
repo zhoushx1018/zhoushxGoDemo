af48d00cb8ba18b27d567a491833cb4a3af69f27
zhoushengxian <zhoushengxian@richinfo.cn>
2017-02-17 17:17:25 +0800

    新增项目  godepDemo

---
+++ godepDemo/main.go
@@ -0,0 +1,15 @@
+//无坑安装使用godep(go语言第三方依赖管理)
+//http://studygolang.com/articles/2147
+package main
+
+import (
+	"fmt"
+
+	"xx.org/dep"
+)
+
+func main() {
+	//    fmt.Printf("Hello, world!\n")
+	fmt.Printf("1111111Hello, world!\n")
+	external.Show()
+}
